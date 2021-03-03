d6125e478f6366592c87370652fa7e8cb66d548f
Ronaldo Albertini <ronaldo.albertini@omni.com.br>
2021-03-02 22:26:02 -0300

    Initial project

---
+++ Card Phrases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card Phrases
+//
+//  Created by Tomate Albertini on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
